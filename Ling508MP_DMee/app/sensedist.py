ed1f3bff8e810ce8963f81c0e2aa26920022e465
dlmee <dillonlornemee@email.arizona.edu>
2022-07-25 00:40:05 -0400

    This is my initial commit. It has three tests, one for each class, which pass on my local machine. I will add a dockerfile in the next few days.

---
+++ Ling508MP_DMee/app/sensedist.py
@@ -0,0 +1,149 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from bs4 import BeautifulSoup
+import re
+from nltk.corpus import stopwords
+from collections import Counter
+
+def scrapePG(url):
+    try:
+        source = urlopen(url)
+    except HTTPError as e:
+        print(e)
+    except URLError as e:
+        print(e)
+    else:
+        bs = BeautifulSoup(source.read(), 'html.parser')
+        paragraphs = []
+
+        for paragraph in bs.div.find_all('p', class_=False):
+            if len(paragraph.get_text()) > 4:
+                paragraphs.append(paragraph.get_text())
+        paragraphs = paragraphs[0:-23]
+        sentences = []
+        for paragraph in paragraphs:
+            paragraph = re.sub('[\n\"\-\',\[\d\]]', ' ', paragraph)
+            sents = re.split('[\.\!\?]', paragraph)
+            for s in sents:
+                sentences.append(s)
+        return sentences
+
+#Input, a word. Output: a lemma
+class Word:
+    def __init__(self, word, lexicon):
+        self.surface = word
+        self.listlexicon(lexicon)
+        self.findlemma(word)
+    def listlexicon(self, lexicon):
+        lexicon = [line.lower() for line in lexicon]
+        lexset = set()
+        for paragraph in lexicon:
+            allwords = re.findall('\w+', paragraph)
+            for words in allwords:
+                lexset.add(words)
+        self.lex = list(lexset)
+        self.lex = sorted(self.lex)
+        #self.wordflow = ' '.join([str(w) for w in self.lex])
+
+    def findlemma(self, word):
+        lemmatch = []
+        poslemma = [word]
+        for i in range(len(word)):
+            if len(word)-i > 2:
+                poslemma.append(word[0:-1-i])
+        print(poslemma)
+        plemmas = {}
+
+        for pl in poslemma:
+            lm = []
+            for posword in self.lex:
+                if re.match(pl, posword):
+                    lm.append(posword)
+                plemmas[pl] = lm
+            if len(pl) == 4:
+                self.lemma = pl
+                self.inflections = plemmas[pl]
+        #print(plemmas)
+
+#Initially I had a Lemma class, but I'm not sure if I need that, because I'm finding that using the inflections is how I find the lemma.
+
+#Input, list of inflections. Output: list of target vectors
+class Vector:
+    def __init__(self, lemma, inflections, vectors):
+        self.lemma = lemma
+        self.inflections = inflections
+        self.sourcevectors = vectors
+        self.targvectors = []
+        self.findtargets()
+
+    def findtargets(self):
+
+        for word in self.inflections:
+            self.targvectors.append((word, self.sourcevectors[word]))
+
+#Input, list of tokenized sentences. Output: List of dictionary of dictionaries
+#Used code from my internship to run. The beauty of classes emerges! A microservice applied in different contexts.
+
+class Vectorizer:
+    def __init__(self, corpus):
+        self.corpus = corpus
+        self.BISW = stopwords.words('english')
+        self.tokenize()
+        self.dictasvector()
+
+    def tokenize(self):
+        self.refine = [line.lower() for line in self.corpus]
+        self.refine = [re.findall('\w+', line) for line in self.refine]
+        self.nrefine = self.stopwords(self.refine)
+
+    def stopwords(self, listoftokens):
+        newlist = []
+        for line in listoftokens:
+            newlist.append([token for token in line if token not in self.BISW])
+        return list(newlist)
+
+    def merge_nest_dict(self, dictword1, dictword2):
+        dict1 = Counter(dictword1)
+        dict2 = Counter(dictword2)
+        dict3 = dict1 + dict2
+
+        return (dict3)
+
+    def dictasvector(self):
+        self.allvector = {}
+        tempvector = {}
+        for line in self.nrefine:
+            for word in line:
+                if word in self.allvector:
+                    nest = {}
+                    for w2 in line:
+                        if w2 != word:
+                            nest[w2] = 1
+                    tempvector[word] = nest
+                    self.allvector[word] = self.merge_nest_dict(self.allvector[word], tempvector[word])
+                else:
+                    nest = {}
+                    for w2 in line:
+                        if w2 != word:
+                            nest[w2] = 1
+                    self.allvector[word] = nest
+
+
+
+
+if __name__ == '__main__':
+    url = 'http://localhost:8000/GM_TPaC.html'
+    texts = scrapePG(url)
+    vectors = Vectorizer(texts)
+    newword = Word('believe', texts)
+    #print(newword.lex)
+    #print(newword.lemma)
+    print(vectors.allvector['crown']['king'])
+    print(type(vectors.allvector))
+    tvec = Vector(newword.lemma, newword.inflections, vectors.allvector)
+    print(tvec.targvectors[0])
+    print(type(tvec.targvectors[0]))
+    for tup in tvec.targvectors:
+        print(tup)
+
---
+++ Ling508MP_DMee/tests/test_sensedist.py
@@ -0,0 +1,28 @@
+from app.sensedist import *
+
+def test_Word():
+    word = 'believe'
+    url = 'http://localhost:8000/GM_TPaC.html'
+    texts = scrapePG(url)
+    lm = Word('believe', texts)
+    assert type(lm.lemma) == str
+    assert type(lm.inflections) == list
+    assert len(lm.inflections) > 0
+
+def test_Vectorizer():
+    url = 'http://localhost:8000/GM_TPaC.html'
+    texts = scrapePG(url)
+    vectors = Vectorizer(texts)
+    assert type(vectors.allvector) == dict
+    assert type(vectors.allvector['crown']['king']) == int
+
+def test_Vector():
+    url = 'http://localhost:8000/GM_TPaC.html'
+    texts = scrapePG(url)
+    vectors = Vectorizer(texts)
+    word = Word('believe', texts)
+    tvec = Vector(word.lemma, word.inflections, vectors.allvector)
+    assert type(tvec.targvectors) == list
+    assert type(tvec.targvectors[0]) == tuple
+    assert type(tvec.targvectors[0][0]) == str
+    assert tvec.targvectors[0][1]['knew'] == 1
